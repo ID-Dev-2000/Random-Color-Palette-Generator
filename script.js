d0c9d1d8603f777eb1a5264a52dd574e4e215b80
ID <izaakduncan@outlook.com>
2022-06-02 03:27:01 -0700

    error handling

--- script.js
+++ script.js
@@ -11,17 +11,19 @@ async function fetchImages() {
     let fetchedData = await fetch(`https://www.thecolorapi.com/scheme?hex=${randomColor}&mode=triad&count=6`, {
         headers: {"Accept" : "application/json"},
     })
-    if (fetchedData.ok == false) 
-    {
-        alert("API is probably on cooldown")
-    }else{
     let fetchedDataAsJSON = await fetchedData.json()
-    colorImage.src = fetchedDataAsJSON.image.bare
-    imageHex1.innerHTML = fetchedDataAsJSON['colors']['0']['hex']['value']
-    imageHex2.innerHTML = fetchedDataAsJSON['colors']['1']['hex']['value']
-    imageHex3.innerHTML = fetchedDataAsJSON['colors']['2']['hex']['value']
-    imageHex4.innerHTML = fetchedDataAsJSON['colors']['3']['hex']['value']
-    imageHex5.innerHTML = fetchedDataAsJSON['colors']['4']['hex']['value']
-    imageHex6.innerHTML = fetchedDataAsJSON['colors']['5']['hex']['value']
+    if (fetchedData.ok == false) {
+        alert("API is probably on cooldown")
+    }else if(fetchedDataAsJSON['colors']['0']['hex']['value'] == '#000000') {
+        fetchImages() // re-calls function if value display error is true
+    } else {
+        colorImage.src = fetchedDataAsJSON.image.bare
+        console.log(fetchedDataAsJSON)
+        imageHex1.innerHTML = fetchedDataAsJSON['colors']['0']['hex']['value']
+        imageHex2.innerHTML = fetchedDataAsJSON['colors']['1']['hex']['value']
+        imageHex3.innerHTML = fetchedDataAsJSON['colors']['2']['hex']['value']
+        imageHex4.innerHTML = fetchedDataAsJSON['colors']['3']['hex']['value']
+        imageHex5.innerHTML = fetchedDataAsJSON['colors']['4']['hex']['value']
+        imageHex6.innerHTML = fetchedDataAsJSON['colors']['5']['hex']['value']
     }
 }
